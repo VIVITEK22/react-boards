1721d72bca10e1e347ca0dc5b47609e1bcf074f0
VIVITEK <baiborodovxxx@gmail.com>
2025-05-28 20:54:31 +0500

    fix: add base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig({
-  base: "/react-boards/",
-  plugins: [react(), viteTsconfigPaths(), tailwindcss()],
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/react-boards/" : "/";
+  
+  return {
+    base,
+    plugins: [react(), viteTsconfigPaths(), tailwindcss()],
+  };
 });
